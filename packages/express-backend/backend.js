14cfa38c62113ab5194b753f84e1c4213f488aab
Samiksha Gollamudi Karimbil <samiksha.gk29@gmail.com>
2024-04-10 13:27:34 -0700

    step 5

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -38,6 +38,19 @@ const users = {
       (user) => user["name"] === name
     );
   };
+
+  const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
   
   app.get("/users", (req, res) => {
     const name = req.query.name;
