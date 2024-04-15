96bb48707776052805dead285070e5ad1d052ef0
Samiksha Gollamudi Karimbil <samiksha.gk29@gmail.com>
2024-04-14 22:28:30 -0700

    committing exercise 2

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -48,23 +48,6 @@ const users = {
     ]
 };
 
-const findUserByName = (name) => {
-    return users["users_list"].filter(
-      (user) => user["name"] === name
-    );
-};
-
-app.get("/users", (req, res) => {
-    const name = req.query.name;
-    if (name != undefined) {
-        let result = findUserByName(name);
-        result = { users_list: result };
-        res.send(result);
-    } else {
-        res.send(users);
-    }
-});
-
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 
@@ -104,6 +87,12 @@ app.delete("users/:id", (req, res) => {
     }
 });
 
+const findUserByName = (name) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name
+    );
+};
+
 const findUserByNameAndJob = (name, job) => {
     return users["users_list"].filter(
         (user) => user["name"] === name && user["job"] === job
@@ -113,15 +102,17 @@ const findUserByNameAndJob = (name, job) => {
 app.get("/users", (req, res) => {
     const name = req.query.name;
     const job = req.query.job;
-    if (name != undefined) {
-        if (job != undefined){
-            let result = findUserByNameAndJob(name, job);
-            result = { users_list: result };
-            res.send(result);
-        }
-        
-        
-    } else {
+    if (name != undefined && job == undefined){
+        let result = findUserByName(name);
+        result = { users_list: result };
+        res.send(result);
+    }
+    else if (name != undefined && job != undefined){
+        let result = findUserByNameAndJob(name, job);
+        result = { users_list: result };
+        res.send(result);
+    }
+    else {
         res.send(users);
     }
 });
