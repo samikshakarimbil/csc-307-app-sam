d75a2df650fc7bbb9737870aa8f08512c9a3f8ef
Samiksha Gollamudi Karimbil <samiksha.gk29@gmail.com>
2024-04-14 22:39:50 -0700

    ex 3 step 3

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -13,9 +13,13 @@ function MyApp() {
     setCharacters(updated);
   }
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
-  }
+  function updateList(person) { 
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      })
+}
 
   function fetchUsers() {
     const promise = fetch("http://localhost:8000/users");
@@ -28,6 +32,18 @@ function MyApp() {
       .then((json) => setCharacters(json["users_list"]))
       .catch((error) => { console.log(error); });
   }, [] );
+
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
 
   return (
     <div className="container">
