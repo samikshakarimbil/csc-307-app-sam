5176460e562dc0d5f8a673e329cc880c300ec668
Samiksha Gollamudi Karimbil <samiksha.gk29@gmail.com>
2024-04-14 22:37:53 -0700

    ex 3 step 2

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,5 +1,5 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
+import React, {useState, useEffect} from 'react';
 import Table from "./Table";
 import Form from "./Form";
 
@@ -17,11 +17,17 @@ function MyApp() {
     setCharacters([...characters, person]);
   }
 
-  
-  // function fetchUsers() {
-  //   const promise = fetch("http://localhost:8000/users");
-  //   return promise;
-  // }
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => { console.log(error); });
+  }, [] );
 
   return (
     <div className="container">
