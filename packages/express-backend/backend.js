02c3e3840b9f969fa2d1bfd661bab9a040b8ec62
Samiksha Gollamudi Karimbil <samiksha.gk29@gmail.com>
2024-04-17 16:11:45 -0700

    ex 3 step 4 - 2

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -61,15 +61,20 @@ app.get("/users/:id", (req, res) => {
     }
 });
 
+function generateID() {
+  return Math.random().toString(16).substr(2, 6);
+}
+
 const addUser = (user) => {
-    users["users_list"].push(user);
-    return user;
+  user.id = generateID();
+  users["users_list"].push(user);
+  return user;
 };
   
 app.post("/users", (req, res) => {
-    const userToAdd = req.body;
-    addUser(userToAdd);
-    res.status(201).send("User added successfully");
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.status(201).send("User added successfully");
 });
 
 const delUser = (id) => {
